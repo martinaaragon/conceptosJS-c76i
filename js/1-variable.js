f8e1f9e8ea72252bbc76f1b8afcee259b523f408
Aragon Martina <martinaaragon6@gmail.com>
2023-10-12 18:32:49 -0300

    agregamos un comentario y una variable

---
+++ js/1-variable.js
@@ -0,0 +1,10 @@
+//esto es un comentario de una linea
+
+/*
+esto es un comentario
+de multiples
+lineas
+*/
+
+// declarar una variable (var - let; puedo cambiar su valor - const; jamas podre cambiar su valor)
+let anioActual = 2023
