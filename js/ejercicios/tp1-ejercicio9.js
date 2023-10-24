6e750aa9013f0d86ecf08e98b2cb9f168e0f57bf
Aragon Martina <martinaaragon6@gmail.com>
2023-10-24 18:34:50 -0300

    trabajamos el ejercicio 9 con bucles

---
+++ js/ejercicios/tp1-ejercicio9.js
@@ -0,0 +1,13 @@
+//9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+//Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+
+let frase = prompt('ingrese una frase');
+console.log(frase.length);
+console.log(frase.charAt(0));
+
+//hola mundo
+//escribe un programa 
+if(frase.charAt(0) === 'a' || frase.charAt(0) === 'e' || frase.charAt(0) === 'i' || frase.charAt(0) || frase.charAt(0) 
+=== 'u'){
+         document.write(frase.charAt(0))
+}
