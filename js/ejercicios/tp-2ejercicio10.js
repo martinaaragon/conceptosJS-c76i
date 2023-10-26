ba35b0f90b3f013adcca9022336e28c0189dd162
Aragon Martina <martinaaragon6@gmail.com>
2023-10-26 18:15:01 -0300

    dibujamos la cantidad de ilas indicada por el usuario

---
+++ js/ejercicios/tp-2ejercicio10.js
@@ -0,0 +1,17 @@
+const filas = parseInt(prompt('Ingrese un número de filas'));
+const columnas = parseInt(prompt('Ingrese un número de columnas'));
+
+const totalCeldas = filas * columnas;
+
+document.write(`<table> <tbody>`);
+for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
+document.write(`<tr>
+              
+                        <td>35</td>
+                        <td>34</td>
+                        <td>33</td>
+                        
+
+</tr>`);
+}
+document.write( `</table></tbody`) ;
